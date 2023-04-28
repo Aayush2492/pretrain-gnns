fc865a7fc69518da6ef2d7096bebf9102c737165
Nandini <>
2023-04-28 14:33:56 +0530

    Pool node embeddings to get the graph embedding

--- subgraph_iso/loader.py
+++ subgraph_iso/loader.py
@@ -28,65 +28,83 @@ class CustomDataset(InMemoryDataset):
         # nodes
         node_dict = {}
 
-        edge_list = [
-            (0, 0, 1, 1),
-            (0, 0, 2, 2),
-            (1, 1, 2, 2),
-            (1, 1, 3, 3),
-            (2, 2, 3, 3),
-            (2, 2, 4, 4),
-            (3, 3, 4, 4),
-            (3, 3, 5, 5),
-            (4, 4, 5, 5),
+        edge_lists = [
+            [
+                (0, 0, 1, 1),
+                (0, 0, 2, 2),
+                (1, 1, 2, 2),
+                (1, 1, 3, 3),
+                (2, 2, 3, 3),
+                (2, 2, 4, 4),
+                (3, 3, 4, 4),
+                (3, 3, 5, 5),
+                (4, 4, 5, 5),
+                (4, 4, 6, 6),
+            ],
+            [
+                (0, 0, 1, 1),
+                (0, 0, 2, 2),
+                (1, 1, 2, 2),
+                (1, 1, 3, 3),
+                (2, 2, 3, 3),
+                (2, 2, 4, 4),
+                (3, 3, 4, 4),
+                (3, 3, 5, 5),
+            ],
         ]
 
-        for edge in edge_list:
-            src, src_label, dst, dst_label = edge
-            if node_dict.get(src) is None:
-                node_dict[src] = src_label
-            if node_dict.get(dst) is None:
-                node_dict[dst] = dst_label
+        data_list = []
+        for index, edge_list in enumerate(edge_lists):
+            for edge in edge_list:
+                src, src_label, dst, dst_label = edge
+                if node_dict.get(src) is None:
+                    node_dict[src] = src_label
+                if node_dict.get(dst) is None:
+                    node_dict[dst] = dst_label
 
-        node_list = list(node_dict.keys())
-        node_list = [[elem, 1] for elem in node_list]
-        print("xxxx", node_list)
-        x = torch.tensor(np.array(node_list), dtype=torch.long)
-        print("xxxx", x.shape)
+            node_list = list(node_dict.keys())
+            node_list = [[elem, 1] for elem in node_list]
+            print("Node features", node_list)
+            x = torch.tensor(np.array(node_list), dtype=torch.long)
+            print("xxxx", x.shape)
 
-        # edges
-        edges_list = []
-        edge_features_list = []
-        for edge in edge_list:
-            src, src_label, dst, dst_label = edge
-            i = src
-            j = dst
-            edge_feature = [1, 1]  # edge label
-            edges_list.append((i, j))
-            edge_features_list.append(edge_feature)
-            edges_list.append((j, i))
-            edge_features_list.append(edge_feature)
+            # edges
+            edges_list = []
+            edge_features_list = []
+            for edge in edge_list:
+                src, src_label, dst, dst_label = edge
+                i = src
+                j = dst
+                edge_feature = [1, 1]  # edge label
+                edges_list.append((i, j))
+                edge_features_list.append(edge_feature)
+                edges_list.append((j, i))
+                edge_features_list.append(edge_feature)
 
-        # data.edge_index: Graph connectivity in COO format with shape [2, num_edges]
-        edge_index = torch.tensor(np.array(edges_list).T, dtype=torch.long)
+            # data.edge_index: Graph connectivity in COO format with shape [2, num_edges]
+            edge_index = torch.tensor(np.array(edges_list).T, dtype=torch.long)
 
-        # data.edge_attr: Edge feature matrix with shape [num_edges, num_edge_features]
-        edge_attr = torch.tensor(np.array(edge_features_list), dtype=torch.long)
+            # data.edge_attr: Edge feature matrix with shape [num_edges, num_edge_features]
+            edge_attr = torch.tensor(np.array(edge_features_list), dtype=torch.long)
 
-        data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr)
-        data.id = torch.tensor([0])
-        #         data.y = torch.tensor([labels[i]])
+            data = Data(x=x, edge_index=edge_index, edge_attr=edge_attr)
+            data.id = torch.tensor([i])
+            #         data.y = torch.tensor([labels[i]])
 
-        print("Data", data, data.edge_index.dtype)
+            print("Data", data, data.edge_index.dtype)
 
-        # Apply pre-processing transformations, if any
-        # if self.pre_transform is not None:
-        #     data = self.pre_transform(data)
+            # Apply pre-processing transformations, if any
+            # if self.pre_transform is not None:
+            #     data = self.pre_transform(data)
 
-        # Save the processed data to disk
-        # torch.save(self.collate([data]), self.processed_paths[0])
-        data_list = [data]
+            # Save the processed data to disk
+            # torch.save(self.collate([data]), self.processed_paths[0])
+            data_list.append(data)
+            # break
+
         data, slices = self.collate(data_list)
         torch.save((data, slices), self.processed_paths[0])
+
 
 # import os
 # import torch
@@ -439,7 +457,7 @@ class CustomDataset(InMemoryDataset):
 #             ### get downstream test molecules.
 #             from splitters import scaffold_split
 
-#             ### 
+#             ###
 #             downstream_dir = [
 #             'dataset/bace',
 #             'dataset/bbbp',
@@ -806,7 +824,7 @@ class CustomDataset(InMemoryDataset):
 #                     data.y = torch.tensor([labels[i]])
 #                     data_list.append(data)
 #                     data_smiles_list.append(smiles_list[i])
-                    
+
 
 #         else:
 #             raise ValueError('Invalid dataset name')
@@ -1415,4 +1433,3 @@ class CustomDataset(InMemoryDataset):
 # if __name__ == "__main__":
 
 #     create_all_datasets()
-
